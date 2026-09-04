740c4d8d212a3e15dc7693b3675725a0bad6c5e5
mkdior <h@mza.jp>
2026-09-04 18:40:18 +0900

    ~ -- bump version to v0.11.1

--- cmd/ftv/main.go
+++ cmd/ftv/main.go
@@ -5,7 +5,7 @@ import "github.com/mkdior/FastTableViewer/internal/app"
 
 // version is the release version; overridden at build time with
 // -ldflags "-X main.version=...".
-var version = "0.11.0"
+var version = "0.11.1"
 
 func main() {
 	app.Execute(version)
